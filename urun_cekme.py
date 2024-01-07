4ddf17ece9e722d3f33960e46560f828a9b14b9f
Enes KOC <eneskoc.0063@gmail.com>
2024-01-07 23:03:57 +0300

    Yorumlara geçme kısmı yapıldı

--- urun_cekme.py
+++ urun_cekme.py
@@ -1,6 +1,8 @@
 import csv
+import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 
 linkler = []
 
@@ -25,3 +27,11 @@ for i in range(len(linkler)):
     urunFiyat = bs.find("span", attrs={"class": "product-list__price"}).text
     urunMarka = bs.find("h1", attrs={"class": "product-list__product-name"}).text
     urunYildiz = (bs.find('span', attrs={"class": "score"})).get("style")
+
+    yorumlar_linki = browser.find_element(By.CSS_SELECTOR, 'a[href="#yorumlar"]')
+    yorumlar_linki.click()
+    time.sleep(3)
+    #urunYorumlar = bs.find("div", attrs={"class": "col-md-12 ds-table comment-items"})
+    #yorum = urunYorumlar.find_all("span")
+    #text = bs.find('div', class_='comment-section')
+    #print(yorum)
