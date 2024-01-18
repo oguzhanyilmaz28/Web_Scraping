5d460b0f936a009a198d7c85b3eb2867ae3a710c
Oğuzhan Yılmaz <101720462+oguzhanyilmaz28@users.noreply.github.com>
2024-01-18 16:58:09 +0300

    Update link_cekme.py

--- link_cekme.py
+++ link_cekme.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 
+#sitenin ana sayfasından baslayarak cep telefonu kısmının bulunması
 anaurl = "https://www.vatanbilgisayar.com/"
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 browser.get(anaurl)
@@ -17,7 +18,7 @@ time.sleep(1)
 tümtelefon = browser.find_element(By.XPATH, '//*[@id="navbar"]/ul/li[1]/div/div/div[1]/ul[2]/div[1]/li[1]/a')
 tümtelefon.click()
 current_url = browser.current_url
-
+browser.close()
 
 sayfa_url_liste = []
 urun_url_liste=[]
@@ -27,38 +28,22 @@ for a in range(9):
     sayfa_url_liste.append(str(current_url)+"?page=" + str(sayac))
     sayac += 1
 
-#tarayıcı günlüklerini devre dışı bırakır, gereksiz log çıktılarından kaçınır
-options = webdriver.ChromeOptions();
-options.add_experimental_option('excludeSwitches', ['enable-logging']);
-
 
 def scrape_link(url):
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     browser.get(url)
-
     time.sleep(3)
 
-    for c in range(1,25) :
-        urun_url_liste.append(
-            browser.find_element(By.XPATH, '//*[@id="productsLoad"]/div['+str(c)+']/div[1]/a').get_attribute('href'))
-        c+=1
+#urun url lınklerının alınması
+    for c in range(1, 25):
+        urun_url = browser.find_element(By.XPATH, '//*[@id="productsLoad"]/div[' + str(c) + ']/div[1]/a').get_attribute('href')
+        urun_url_liste.append([urun_url])
 
-    f = open('link.csv', 'w', newline='', encoding="utf-8")
-    baslik = ["link"]
-    writer = csv.writer(f)
-    writer.writerow(baslik)
-    for row in urun_url_liste:
-        writer.writerow(row)
-    f.close()
-
-    with open('link.csv', 'r') as dosya:
-        veri = dosya.read()
-
-    temiz_veri = veri.replace(',', '')
-
-    with open('link.csv', 'w') as temizlenmis_dosya:
-        temizlenmis_dosya.write(temiz_veri)
-
+#csv dosyasına link kaydediliyor
+    with open('link.csv', 'w', newline='', encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["link"])
+        writer.writerows(urun_url_liste)
 
 #kac sayfa gezecek o belirtilir
 for x in range(9):
