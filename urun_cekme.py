84553012e2082b86424a54438c74fc65c9a22e46
Enes KOC <92601067+EnesKoc10@users.noreply.github.com>
2024-01-18 16:58:51 +0300

    Degisikler yapildi

--- urun_cekme.py
+++ urun_cekme.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 # Programın başlangıç zamanını al
 start_time = time.time()
 
-con = sqlite3.connect("urun.db")
+con = sqlite3.connect("ceptelefon.db")
 cursor = con.cursor()
 
 # Tablo varsa silme kodu
@@ -17,22 +17,23 @@ cursor.execute("DROP TABLE IF EXISTS yorum")
 
 def tabloOlustur():
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS urun (urunId INTEGER PRIMARY KEY AUTOINCREMENT,"
+    cursor.execute("CREATE TABLE IF NOT EXISTS Urun (urunID INTEGER PRIMARY KEY AUTOINCREMENT,"
                        "adi TEXT NOT NULL,"
                        "marka TEXT NOT NULL,"
+                       "kategori TEXT NOT NULL,"
                        "stok TEXT NOT NULL,"
                        "fiyat INTEGER,"
                        "link TEXT NOT NULL,"
                        "ort_yildiz REAL,"
                        "yorum_sayisi INTEGER)")
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS yorum (yorumId INTEGER PRIMARY KEY AUTOINCREMENT,"
+    cursor.execute("CREATE TABLE IF NOT EXISTS Yorum (yorumID INTEGER PRIMARY KEY AUTOINCREMENT,"
                        "isim TEXT,"
                        "yorum TEXT,"
                        "tarih TEXT,"
                        "yildiz INTEGER ,"
-                       "urunId INTEGER ,"
-                       "FOREIGN KEY (urunId) REFERENCES urun(urunId))")
+                       "urunID INTEGER ,"
+                       "FOREIGN KEY (urunID) REFERENCES Urun(urunID))")
 
 
 tabloOlustur()
@@ -58,10 +59,11 @@ for i in range(len(linkler)):
     bs = BeautifulSoup(kaynak, "html.parser")
 
     urunBilgi = bs.findAll("a", attrs={"class": "bradcrumb-item"})
+    urunKategori = urunBilgi[2].text
     urunMarka = urunBilgi[3].text
     urunModel = urunBilgi[4].text
     toplamYorum = int((bs.find("a", attrs={"class": "comment-count"}).text).replace("(", "").replace(")", ""))
-    stokBilgi = bs.find("div", attrs={"class": "d-cell product-button--cell"}).text  #class="btn btn-success detail-cart-button btn-nonstock"
+    stokBilgi = bs.find("div", attrs={"class": "d-cell product-button--cell"}).text
 
     if stokBilgi == "\n\nTükendi\n\n":
         stokDurum = "Yok"
@@ -70,11 +72,10 @@ for i in range(len(linkler)):
         stokDurum = "Var"
         urunFiyat = bs.find("span", attrs={"class": "product-list__price"}).text
 
-    print(urunFiyat)
     try:
         yorumlar_linki = browser.find_element(By.CSS_SELECTOR, 'a[id="allCommentBtn"]')
         yorumlar_linki.click()
-        time.sleep(1)
+        time.sleep(3)
     except:
         print("Yorum Yok")
 
@@ -82,16 +83,12 @@ for i in range(len(linkler)):
     bs = BeautifulSoup(kaynak, "html.parser")
     ortalamaRank = float(bs.find("strong", attrs={"id": "averageRankNum"}).text)
 
-    connection = sqlite3.connect("urun.db")
-    cursor = connection.cursor()
-
-    sql_sorgu = "INSERT INTO urun (adi,marka,stok, fiyat,link,ort_yildiz,yorum_sayisi) VALUES ( ?, ?, ?, ?, ?, ?, ?)"
-    veri = (urunModel, urunMarka, stokDurum, urunFiyat, linkler[i], ortalamaRank, toplamYorum)
+    sql_sorgu = "INSERT INTO urun (adi,marka, kategori, stok, fiyat,link,ort_yildiz,yorum_sayisi) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)"
+    veri = (urunModel, urunMarka,  urunKategori, stokDurum, urunFiyat, linkler[i], ortalamaRank, toplamYorum)
 
     # Veritabanına ekleme işlemi
     cursor.execute(sql_sorgu, veri)
-    connection.commit()
-    connection.close()
+    con.commit()
 
     urunYorumlar = bs.find("div", attrs={"class": "comment-section"})
 
@@ -103,18 +100,17 @@ for i in range(len(linkler)):
     #ilk 100 yorumu alması için
     yorumlar = yorumlar[0:100]
 
-    connection = sqlite3.connect("urun.db")
-    cursor = connection.cursor()
-
     for a in range(len(yorumlar)):
         sql_sorgu = "INSERT INTO yorum (isim, yorum, tarih, yildiz,urunId) VALUES ( ?, ?, ?, ?, ?)"
         veri = (isimler[a].text, yorumlar[a].text, tarihler[a].text, rank[a].get('data-rank'), i+1)
         # Veritabanına ekleme işlemi
         cursor.execute(sql_sorgu, veri)
 
-    # Değişiklikleri kaydet ve bağlantıyı kapat
-    connection.commit()
-    connection.close()
+    # Değişiklikleri kaydet
+    con.commit()
+
+# Veritabanin kapatilmasi
+con.close()
 
 # Programın bitiş zamanını al
 end_time = time.time()
